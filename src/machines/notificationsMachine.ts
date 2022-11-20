e0843bbac3f0e049d9776cca83dbc027fead1aa3
dinavinter <dina.vintr@gmail.com>
2022-11-20 22:25:28 +0200

    Initial commit

---
+++ src/machines/notificationsMachine.ts
@@ -0,0 +1,56 @@
+ import {actions, createMachine, InterpreterFrom, MachineConfig} from "xstate";
+const  {assign} =actions;
+
+ export type ApiNotificationResponseItem ={
+     payload: any,
+ }
+
+ export type NotificationResponseItem =ApiNotificationResponseItem &{
+     severity?: "success" | "info" | "warning" | "error";
+     title: string,
+     id: string
+
+ }
+export interface NotificationsSchema {
+
+    states: {
+        visible: {};
+    };
+}
+
+export type NotificationsEvents = { type: "ADD", notification: NotificationResponseItem } | { type: "HIDE" };
+
+
+export interface NotificationsContext {
+    notifications: Array<NotificationResponseItem>
+
+}
+
+export const notificationsMachineConfig: MachineConfig<NotificationsContext, NotificationsSchema, NotificationsEvents> = {
+    context: {
+        notifications: Array.of<NotificationResponseItem>()
+    },
+    initial: "visible",
+    states: {
+        visible: {
+            on: {
+                'ADD': {
+                    actions: "addNotification"
+                }
+            }
+        }
+    }
+};
+
+
+export const notificationMachine= createMachine(notificationsMachineConfig, {
+    actions: {
+        addNotification:  assign({
+            notifications: (context, event: NotificationsEvents)=> {
+                return event.type === "ADD" ? [...context.notifications , event.notification]: context.notifications
+            }
+        })
+    }
+})
+
+export type NotificationsService = InterpreterFrom<typeof notificationMachine>
